5766537bcdceebe3b67003ef07c650a49762c868
KondalDurgam <Kondaldurgam733@gmail.com>
2020-10-17 17:21:51 +0530

    1 twosum javascript solution

---
+++ Alogorithms/Hash/easy/1.TwoSum.js
@@ -0,0 +1,35 @@
+// Solution  O(n)
+var twoSum = function(nums, target) {
+    var map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let comp = target - nums[i];
+        if (map.has(nums[i])) {
+            return [map.get(nums[i]), i];
+        }
+        map.set(comp, i);
+    }
+};
+
+// Solution  O(n)
+var twoSum = function(nums, target) {
+    var minValue = 0;
+    for (var i = 1; i < nums.length; i++) {
+        if (nums[minValue] + nums[i] === target) {
+            return [minValue, i];
+        } else if (i === nums.length - 1) {
+            minValue++;
+            i = minValue;
+        }
+    }
+};
+
+// Solution  O(n)
+var twoSum = function(nums, target) {
+    var comp = {};
+    for (var i = 0; i < nums.length; i++) {
+        if (comp[nums[i]] >= 0) {
+            return [comp[nums[i]], i];
+        }
+        comp[target - nums[i]] = i;
+    }
+};
