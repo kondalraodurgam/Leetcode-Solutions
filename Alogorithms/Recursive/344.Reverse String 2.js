9f8fb9b26c8c17c1065697f4cf771d1d7cd191ed
KondalDurgam <Kondaldurgam733@gmail.com>
2020-10-22 05:11:08 +0530

    Implemented Recursive Alogorithms solutions

---
+++ Alogorithms/Recursive/344.Reverse String 2.js
@@ -0,0 +1,23 @@
+var reverseString = function(s, i = 0, j = s.length - 1) {
+    if (i >= j) return s;
+    [s[i], s[j]] = [s[j], s[i]];
+    reverseString(s, i + 1, j - 1)
+};
+
+
+var reverseString = function(s) {
+    function recursiveString(left, right) {
+        if (left < right) {
+            [s[left], s[right]] = [s[right], s[left]];
+            recursiveString(left + 1, right - 1);
+        }
+    }
+    recursiveString(0, s.length - 1)
+};
+
+
+var reverseString = function(s) {
+    for (var i = 0; i < Math.floor(s.length / 2); i++) {
+        [s[i], s[s.length - 1 - i]] = [s[s.length - 1 - i], s[i]]
+    }
+};
