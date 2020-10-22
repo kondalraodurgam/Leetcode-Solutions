40b76bdbb2f278203e4e6853da3070be67207fc0
KondalDurgam <Kondaldurgam733@gmail.com>
2020-10-22 10:14:44 +0530

    1491. Average Salary Excluding the Minimum and Maximum Salary

---
+++ Alogorithms/Array/easy/1491. Average Salary Excluding the Minimum and Maximum Salary.js
@@ -0,0 +1,33 @@
+var average = function(salary) {
+    salary.sort((a, b) => a - b);
+    let arr = [];
+    for (let i = 1; i < salary.length - 1; i++) {
+        arr.push(salary[i]);
+    }
+    return arr.reduce((acc, cur) => acc + cur, 0) / arr.length;
+};
+
+var average = function(salary) {
+    salary.sort((a, b) => a - b);
+    return salary.slice(1, salary.length - 1).reduce((acc, cur) => acc + cur, 0) / (salary.length - 2);
+};
+
+var average = function(salary) {
+    salary.sort((a, b) => a - b);
+    let sum = 0;
+    let len = 0;
+    for (let i = 1; i < salary.length - 1; i++) {
+        sum += salary[i];
+        len++;
+    }
+    return sum / len;
+};
+
+var average = function(salary) {
+    salary.sort((a,b)=>a-b);
+    let sum = 0;
+    for (let i = 1; i < salary.length-1; i++) {
+        sum +=salary[i];
+    }
+    return sum/(salary.length-2);
+};
