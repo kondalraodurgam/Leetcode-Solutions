1e8bf914670298dee406e0446ea06f1b9272b253
KondalDurgam <Kondaldurgam733@gmail.com>
2020-10-23 19:21:06 +0530

    Binary serach implemented

---
+++ Alogorithms/Binary Search/704. Binary Search.js
@@ -0,0 +1,15 @@
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length;
+    while (left <= right) {
+        let mid = left + Math.floor((right - left) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+};
