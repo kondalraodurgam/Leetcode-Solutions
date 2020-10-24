a85470691f316f18aa6525a39c605f03d92339fe
KondalDurgam <Kondaldurgam733@gmail.com>
2020-10-24 09:15:01 +0530

    Binary serach another solution

--- Alogorithms/Binary Search/704. Binary Search.js
+++ Alogorithms/Binary Search/704. Binary Search.js
@@ -13,3 +13,18 @@ var search = function(nums, target) {
     }
     return -1;
 };
+
+var search = function(nums, target) {
+    let left = 0,right = nums.length - 1;
+    while (left <= right) {
+        let mid = left + Math.floor((right - left) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (target < nums[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return -1;
+};
