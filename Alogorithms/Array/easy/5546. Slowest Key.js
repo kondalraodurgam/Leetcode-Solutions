15e5007e7399db4aa0b85db02912c57967375bdb
KondalDurgam <Kondaldurgam733@gmail.com>
2020-10-25 09:24:32 +0530

    5546 .slowestKey solution

---
+++ Alogorithms/Array/easy/5546. Slowest Key.js
@@ -0,0 +1,21 @@
+var slowestKey = function(releaseTimes, keysPressed) {
+    releaseTimes.unshift(0)
+    let str = ""
+    let min = 0;
+    let max = 0;
+    for (let i = 1; i < releaseTimes.length; i++) {
+        max = releaseTimes[i] - releaseTimes[i - 1];
+        if (max > min) {
+            str = keysPressed[i - 1]
+        } else if (max === min) {
+            if (str < keysPressed[i - 1]) {
+                str = keysPressed[i - 1];
+            }
+        }
+        if (max > min) {
+            min = max;
+        }
+
+    }
+    return str;
+};
