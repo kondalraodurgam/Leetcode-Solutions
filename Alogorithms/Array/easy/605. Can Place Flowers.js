c2b5ba8989597d4a552a374c32891fcb42e6a766
KondalDurgam <Kondaldurgam733@gmail.com>
2020-10-27 05:57:41 +0530

    605. Can Place Flowers Javascript solution

---
+++ Alogorithms/Array/easy/605. Can Place Flowers.js
@@ -0,0 +1,9 @@
+var canPlaceFlowers = function(flowerbed, n) {
+    for (let i = 0; i < flowerbed.length && n > 0; i++) {
+        if (flowerbed[i] !== 1 && flowerbed[i - 1] !== 1 && flowerbed[i + 1] !== 1) {
+            flowerbed[i] = 1;
+            n--;
+        }
+    }
+    return n === 0;
+};
