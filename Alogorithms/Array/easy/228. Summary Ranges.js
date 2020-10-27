47ae56dc3da25eb4078e8e378e1d22c514b71e02
KondalDurgam <Kondaldurgam733@gmail.com>
2020-10-27 08:02:13 +0530

    228. Summary Ranges javascript solution

---
+++ Alogorithms/Array/easy/228. Summary Ranges.js
@@ -0,0 +1,15 @@
+var summaryRanges = function(nums) {
+    let arr = [];
+    let startVal = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] + 1 !== nums[i + 1] && nums[i] - 1 !== nums[i - 1]) {
+            arr.push("" + nums[i]);
+        } else if (nums[i] + 1 !== nums[i + 1] && nums[i] - 1 === nums[i - 1]) {
+            arr.push("" + startVal + "->" + nums[i])
+        } else if (nums[i] + 1 === nums[i + 1] && nums[i] - 1 !== nums[i - 1]) {
+            startVal = nums[i]
+        }
+
+    }
+    return arr;
+};
